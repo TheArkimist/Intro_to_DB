cb52b3011bb40cc52883fa3ead6f567e0ee1a77f
Ci$c0 <emmanueloduro37@gmail.com>
2025-10-16 01:59:49 +0000

    Fix formatting of foreign key references in SQL

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -21,13 +21,13 @@ CREATE TABLE Customers (
     );
 CREATE TABLE Orders (
 	order_id INT PRIMARY KEY,
-	customer_id INT, FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+	customer_id INT, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
     );
 CREATE TABLE Order_Details (
 	orderdetailid INT PRIMARY KEY,
 	order_id INT, FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    book_id INT, FOREIGN KEY (book_id) REFERENCES Books (book_id),
+    book_id INT, FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
     );
     
