da44f996b4ab9b649c0de7c6d95fa2a0d18849ad
Ci$c0 <emmanueloduro37@gmail.com>
2025-10-16 02:01:11 +0000

    Fix formatting of foreign key reference in Order_Details

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,7 +26,7 @@ CREATE TABLE Orders (
     );
 CREATE TABLE Order_Details (
 	orderdetailid INT PRIMARY KEY,
-	order_id INT, FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+	order_id INT, FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id INT, FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
     );
